d58650513c8f2482a3db203dde086f586ee90bac
francis-mwas <mwangif184@gmail.com>
2019-06-11 14:20:06 +0300

    Postgresql db configuration

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,19 +8,13 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-    (None, {'fields': ('email', 'password')}),
-    (_('Personal Info'), {'fields': ('name',)}),
-    (
-        _('Permissions'),
-        {
-            'fields': (
-                'is_active',
-                'is_staff',
-                'is_superuser',
-            )
-        }
-    ),
-    (_('Important dates'), {'fields': ('last_login',)}),
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            ('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser',)}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
 
     add_fieldsets = (
@@ -29,4 +23,6 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2')
         }),
     )
+
+
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -4,21 +4,20 @@ from django.urls import reverse
 
 
 class AdminSiteTests(TestCase):
-    
+
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@gmail.com',
             password='admin12345'
         )
-        
+
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
             email='test@londonappdev.com',
             password='password123',
             name='Test User Full Name',
         )
-
 
     def test_users_listed(self):
         """Test that users are listed on user page"""
